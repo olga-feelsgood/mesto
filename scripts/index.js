380764285765cec056765daab0c314ac74409809
Olga Filchukova <makeeva.o.y@gmail.com>
2022-11-06 00:42:12 +0300

    for check full version_4 after firsrt review

--- scripts/index.js
+++ scripts/index.js
@@ -2,37 +2,50 @@ const editProfile = document.querySelector('.profile__edit-button');
 const popupElem = document.querySelector('.popup');
 const popupClose = popupElem.querySelector('.popup__close-button');
 
-editProfile.addEventListener('click', () => {
-  popupElem.classList.add('popup_opened')
-});
-
-popupClose.addEventListener('click', () => {
-  popupElem.classList.remove('popup_opened')
-});
-
-popupElem.addEventListener('click', (event) => {
-  if (event.target === event.currentTarget) {
-    popupElem.classList.remove('popup_opened')
-  }
-});
-
 let formElement = popupElem.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__description');
-
+let nameInput = formElement.querySelector('.popup__input_name');
+let jobInput = formElement.querySelector('.popup__input_description');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
+
+console.log(editProfile);
+console.log(popupElem);
+console.log(popupClose);
+console.log(formElement);
+console.log(nameInput);
+console.log(jobInput);
+console.log(profileName);
+console.log(profileDescription);
+
+function editFormOpen() {
+  popupElem.classList.add('popup_opened');
+}
+
+function editFormClose() {
+  popupElem.classList.remove('popup_opened');
+}
+
+editProfile.addEventListener('click', () => {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDescription.textContent;
+  editFormOpen () ;
+});
+
+popupClose.addEventListener('click', editFormClose);
+
+//popupElem.addEventListener('click', (event) => {
+//  if (event.target === event.currentTarget) {
+//    popupElem.classList.remove('popup_opened')
+//  }
+//});
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
-  let profileName = document.querySelector('.profile__name');
-  let profileDescription = document.querySelector('.profile__description');
-
   profileName.textContent = nameInput.value;
   profileDescription.textContent = jobInput.value;
 
-  popupElem.classList.remove('popup_opened');
+  editFormClose();
 
 }
 
