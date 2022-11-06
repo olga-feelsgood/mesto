0321b9b3b065c0af2d490fde1172b949f7704b7b
Olga Filchukova <makeeva.o.y@gmail.com>
2022-11-06 16:13:33 +0300

    for check full version_5 after second review

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,10 @@ const popupElem = document.querySelector('.popup');
 const popupClose = popupElem.querySelector('.popup__close-button');
 
 let formElement = popupElem.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__input_name');
-let jobInput = formElement.querySelector('.popup__input_description');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_description');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
-
-console.log(editProfile);
-console.log(popupElem);
-console.log(popupClose);
-console.log(formElement);
-console.log(nameInput);
-console.log(jobInput);
-console.log(profileName);
-console.log(profileDescription);
 
 function editFormOpen() {
   popupElem.classList.add('popup_opened');
